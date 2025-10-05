12e6eddadd1ee384173c86b49b6b0b754cba200c
Djebienock <enockdjebi@gmail.com>
2025-10-05 17:21:22 +0100

    Ajout site moderne visiteur

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function initMap() {
+  const fes = { lat: 34.0331, lng: -5.0003 }; // Coordonnées de Fès
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: fes,
+  });
+  new google.maps.Marker({
+    position: fes,
+    map,
+    title: "Bienvenue à Fès 🇲🇦",
+  });
+}
+
 console.log("Bienvenue sur la plateforme touristique de Fès !");
